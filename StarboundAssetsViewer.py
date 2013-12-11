a6b83637f43690a34116ceb0f87f533bb94e7116
clockworkrose <clockwork.rose@gmail.com>
2013-12-10 23:34:18 -0600

    Fills cells

--- StarboundAssetsViewer.py
+++ StarboundAssetsViewer.py
@@ -8,34 +8,50 @@ except ImportError:
     raise ImportError,"The wxPython module is required to run this program"
 
 class MyFile():
-    def __init__(self, parent):
-        self.data = []
-        self.file
+    #def __init__(self):
         
-    def Open(value):
+    def Open(self, value):
         self.file = open(value, 'r+') 
-        data.append(self.file.read())
+        toParse = self.file.read()
+        toParse = toParse.replace("true","\"true\"")
+        toParse = toParse.replace("false","\"false\"")
+        self.data = eval(toParse)
 
-    def HasAttr(value):
-        try:
-            data[value]
-            return true
-        except
-            return false
+    def HasAttr(self, key):
+        return key in self.data
+        
+    #def Save(self):
+        
 
     
 class DispSheet(sheet.CSheet):
     def __init__(self, parent):
         sheet.CSheet.__init__(self, parent)
+        self.Bind(wx.grid.EVT_GRID_CELL_CHANGE, self.OnGridCellChange)
         self.row = self.col = 0
-        self.SetNumberRows(10)
+        self.SetNumberRows(100)
         self.SetNumberCols(2)
         self.SetRowLabelSize(0)
         self.SetColLabelSize(0)
+    
+    def associate(self, file):
+        self.file = file
+        self.populate(file.data)
+    
+    def populate(self, data):
+        index = 0
+        for i in data.iterkeys():
+            self.SetCellValue(index, 0, i)
+            self.SetCellValue(index, 1, str(data[i]))
+            index = index + 1
+        self.SetNumberRows(index-1)
+        self.AutoSize()
 
-    def populate(data):
-        for i in data:
-            self.SetCellValue
+    def OnGridCellChange(self, event):
+        print "you lose"
+       # if hasattr(self, file)
+        
+    
     # def OnGridSelectCell(self, event):
         # self.row, self.col = event.GetRow(), event.GetCol()
         # value =  self.GetColLabelValue(self.col) + self.GetRowLabelValue(self.row)
@@ -51,15 +67,15 @@ class MainApp(wx.Frame):
 
         self.SetSizer(box)
 
-        sheet1 = DispSheet(self)
-        sheet1.SetFocus()
+        self.sheet1 = DispSheet(self)
+        self.sheet1.SetFocus()
         searchbox = wx.TextCtrl(self,-1,value=u"try1",size=(100,20))
         panel2 = wx.Panel(self)
         panel2.SetBackgroundColour('Red')
 
         box.Add(searchbox, (0,0), (1,1), wx.EXPAND)
         box.Add(panel2, (1,0), (1,1), wx.EXPAND)
-        box.Add(sheet1, (0,1), (2,1) ,wx.EXPAND)
+        box.Add(self.sheet1, (0,1), (2,1) ,wx.EXPAND)
 
         self.CreateStatusBar()
         self.Centre()
@@ -67,4 +83,7 @@ class MainApp(wx.Frame):
 
 app = wx.App(0)
 newt = MainApp(None, -1, 'SpreadSheet')
+myfile = MyFile()
+myfile.Open("platinumdrill.miningtool")
+newt.sheet1.associate(myfile)
 app.MainLoop()
