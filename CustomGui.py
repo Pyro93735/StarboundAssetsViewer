61d70c909730ebc7a44eafd66e3ab4eed14e6b18
clockworkrose <clockwork.rose@gmail.com>
2013-12-12 15:59:23 -0600

    Fixed bug with not selecting right. can now open dicts and list by double clicking on them

--- CustomGui.py
+++ CustomGui.py
@@ -60,14 +60,16 @@ class DispSheet(sheet.CSheet):
     def __init__(self, parent):
         sheet.CSheet.__init__(self, parent)
         self.Bind(wx.grid.EVT_GRID_CELL_CHANGE, self.OnGridCellChange)
-        self.Bind(wx.grid.EVT_GRID_SELECT_CELL, self.OnGridCellSelected)
+        self.Bind(wx.grid.EVT_GRID_CELL_LEFT_CLICK, self.OnGridCellSelected)
+        self.Bind(wx.grid.EVT_GRID_CELL_LEFT_DCLICK, self.OnGridCellDClick)
         self.row = self.col = 0
-        self.SetNumberRows(100)
+        self.SetNumberRows(1)
         self.SetNumberCols(2)
         self.SetRowLabelSize(0)
         self.SetColLabelSize(0)
+        self.EnableEditing(True)
         #self.EnableCellEditControl(True)
-        self.EnableGridLines(False)
+        #self.EnableGridLines(False)
 
     def populate(self, node, tree):
         self.node = node
@@ -105,9 +107,24 @@ class DispSheet(sheet.CSheet):
             print "error editing something with no file"
             
     def OnGridCellSelected(self, event):
+        #remove editor
+        self.row = event.GetRow()
+        self.col = event.GetCol()
+        self.SetGridCursor(self.row, self.col)
         self.tree.EnsureVisible(self.children[event.GetRow()])
         self.tree.SelectItem(self.children[event.GetRow()])
-        
+
+    def OnGridCellDClick(self, event):
+        self.row = event.GetRow()
+        self.col = event.GetCol()
+        self.SetGridCursor(self.row, self.col)
+        child = self.children[event.GetRow()]
+        if self.tree.ItemHasChildren(child):
+            self.tree.Expand(child)
+            self.populate(child, self.tree)
+        else:
+            self.EnableCellEditControl()
+            self.ShowCellEditControl()
         
 
 class FileViewer(sheet.CSheet):
